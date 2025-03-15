8827ff6827ff8ca53364a649eba1e123e0c9204d
Bommm <ngongoc0804@gmail.com>
2025-03-15 07:15:26 +0700

    init commit

---
+++ src/dao/ClassSessionDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.util.ArrayList;
+
+public class ClassSession implements Dao<ClassSession>{
+    @Override
+    public int insert(ClassSession classSession) {
+        return 0;
+    }
+
+    @Override
+    public int update(ClassSession classSession) {
+        return 0;
+    }
+
+    @Override
+    public int delete(String id) {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<ClassSession> sellectAll() {
+        return null;
+    }
+
+    @Override
+    public ClassSession sellectById(String id) {
+        return null;
+    }
+}
